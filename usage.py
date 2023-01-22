d7ec381231afdfd4050b9219e4066f730546ce63
Jaeyeon Kim <86578246+JLake310@users.noreply.github.com>
2023-01-23 08:54:41 +0900

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,6 +1,7 @@
 from infer import InferModule
 from keras import models
 
+# 추론 모듈 세팅을 위해 필수적으로 필요한 파라미터들
 model = models.load_model('./model_saved')
 num_per_question = [2,3,4,3,2,3,3,2,3,2,3,2,2,2,2,2]
 num2hobby = {0: '취미1', 1: '취미2', 2: '취미3', 3: '취미4', 4: '취미5'}
