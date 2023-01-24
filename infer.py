95591b03d90f0ac4a0d078c317e347b7d95a453e
Jaeyeon Kim <0310kjy@gmail.com>
2023-01-24 19:44:59 +0900

    [Add] Module 간소화

--- infer.py
+++ infer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 def vectorize_sequences(sequences, dimension=40):
     results = np.zeros((len(sequences), dimension))
@@ -8,15 +9,19 @@ def vectorize_sequences(sequences, dimension=40):
     return results
 
 class InferModule:
-    def __init__(self, model, num_per_question, num2hobby):
+    def __init__(self, model):
         self.model = model
         self.question_bias = []
-        self.num2hobby = num2hobby
-        for idx in range(len(num_per_question)):
+        with open('./model_saved/base_info.json') as f:
+            base_info = json.load(f)
+            self.num_per_question = base_info["list"]
+            self.num2hobby = base_info["hobby_enum"]
+
+        for idx in range(len(self.num_per_question)):
             if idx == 0: 
                 self.question_bias.append(0)
             else:
-                self.question_bias.append(sum(num_per_question[:idx]))
+                self.question_bias.append(sum(self.num_per_question[:idx]))
 
     def start_inferring(self, infer_answer):
         X_infer = [(a+b-1) for a, b in zip(infer_answer, self.question_bias)]
@@ -28,5 +33,5 @@ class InferModule:
             ind = ind[np.argsort(pred[ind])][::-1]
             hobby = []
             for i in ind:
-                hobby.append(self.num2hobby[i])
+                hobby.append(self.num2hobby[str(i)])
             return hobby
--- usage.py
+++ usage.py
@@ -1,12 +1,8 @@
 from infer import InferModule
 from keras import models
 
-# 추론 모듈 세팅을 위해 필수적으로 필요한 파라미터들
 model = models.load_model('./model_saved')
-num_per_question = [2,3,4,3,2,3,3,2,3,2,3,2,2,2,2,2]
-num2hobby = {0: '취미1', 1: '취미2', 2: '취미3', 3: '취미4', 4: '취미5'}
-
-IM = InferModule(model, num_per_question, num2hobby)
+IM = InferModule(model)
 
 if __name__=='__main__':
 
